ed7d1a2013ed14a8d31bcd7a02aa9943d1c6cba5
anagnmar <anagnmar@gmail.com>
2021-11-24 01:11:17 +0200

    Add project files.

---
+++ Management__Leave/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Management__Leave.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
